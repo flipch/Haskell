366fb8068cbb6eafcf691aa8a8ea5657f9ab6e60
kwskii <felipech@outlook.com>
2015-12-01 19:53:11 +0000

    Finished number 5
    Still prints in wrong order but oh well

--- TPC5_47064.hs
+++ TPC5_47064.hs
@@ -1,42 +1,58 @@
 -- | Felipe Heliszkowski, 47064
 
-
 module TP5_47064
-(  ListaFavoritos
+( ListaFavoritos
 , vazia
-,{-- adicionar
+, adicionar
 , marcar
-, extrair--}
+, extrair
 ) where
 
-import Data.List
 
-data ListaFavoritos a = Nil | Cons {listHead :: a, listTail :: ListaFavoritos a}
-                                deriving (Eq, Ord)   
+data ListaFavoritos a = Nil | Cons { listTail :: ListaFavoritos a, listHead :: Item a}
+  deriving (Eq, Ord)
 
-instance (Show a) => Show (ListaFavoritos a) where 
-    show (Nil) = "{}"
-    show (Cons listHead listTail) = show {'
-                                    ++ show listHead 
-                                    ++ show ',  
-                                    ++ show listTail                                                                                                                         
+instance (Show a) => Show (ListaFavoritos a) where
+      show Nil = "{}"
+      show x = "{" ++ toString x ++ "}"
 
-vazia :: ListaFavoritos a 
-vazia = Nil 
+data Item a = Single {value :: a, fav :: Bool} deriving (Eq, Ord)
+
+instance (Show a) => Show (Item a) where
+    show (Single value fav) | fav = show value ++ "*"
+                            | otherwise = show value
+
+toString :: Show a => ListaFavoritos a -> String
+toString (Cons Nil element) = show element
+toString (Cons list element) = toString list ++ "," ++ show element
+
+vazia :: ListaFavoritos a
+vazia = Nil
 
 adicionar :: Eq a => a -> ListaFavoritos a -> ListaFavoritos a
-adicionar = Cons
-            
-{--
+adicionar x y | contains x y = y
+              | otherwise = Cons y (Single x False)
+
+contains :: Eq a => a -> ListaFavoritos a -> Bool
+contains _ Nil = False
+contains x (Cons ys list) | Single x False == list = True
+                          | otherwise = contains x ys
+
 marcar :: Eq a => a -> ListaFavoritos a -> ListaFavoritos a
-marcar a = return ()
+marcar x y | contains x y = popAndMarcar x y Nil
+           | otherwise = y -- Do nothing if it isnt in the list
+
+popAndMarcar :: Eq a => a -> ListaFavoritos a -> ListaFavoritos a -> ListaFavoritos a
+popAndMarcar _ Nil newList = newList
+popAndMarcar x (Cons ys y@(Single val _)) newList
+            | x == val = popAndMarcar x ys (Cons newList (Single x True))
+            | otherwise = popAndMarcar x ys (Cons newList y)
 
 extrair :: ListaFavoritos a -> ListaFavoritos a
-extrair Empty = []
-extrair (Cons x xs) 
-                | x isFavorito = x : extrair xs
-                | otherwise = extrair xs
---}
---isFavorito (Elemento{element = _, favorito = x}) = x
-
-
+extrair Nil = Nil
+extrair (Cons Nil x@(Single _ fav)) = if fav
+  then Cons Nil x
+  else Nil
+extrair (Cons y x@(Single _ fav))
+                          | fav = Cons (extrair y) x
+                          | otherwise = extrair y
